381cf694ca0977480584182404df6db51dd3baf7
Elliot Hipski <elhip03@gmail.com>
2022-06-06 21:58:35 -0400

    Add RPS.js

--- RPS.js
+++ RPS.js
@@ -1,10 +1,7 @@
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 }
-
-let playerChoice = document.getElementById("input");
-
-document.write(playerChoice);
 
 function computerPlay() {
     let choice = getRandomInt(3);
@@ -22,5 +19,48 @@ function computerPlay() {
 }
 
 function gameMaster() {
-    
+
+    let playerChoice = prompt("Rock, Paper, Scissors !!!");
+
+    let gameChoice = computerPlay();
+    console.log("choose rock paper or scissors!");
+
+    let tie = (playerChoice.toLowerCase() == gameChoice.toLowerCase());
+
+    switch(true) {
+
+        case (tie): console.log("tie");
+        break;
+
+        case (playerChoice.toLowerCase() == "scissors" && gameChoice.toLowerCase() == 'paper'): console.log('win');
+        console.log(playerChoice + " beats " + gameChoice);
+        break;
+
+        case (playerChoice.toLowerCase() == "paper" && gameChoice.toLowerCase() == "rock"): console.log("win");
+        console.log(playerChoice + " beats " + gameChoice);
+        break;
+
+        case (playerChoice.toLowerCase() == "rock" && gameChoice.toLowerCase() == "scissors"): console.log("win");
+        console.log(playerChoice + " beats " + gameChoice);
+        break;
+
+        case (playerChoice.toLowerCase() == "scissors" && gameChoice.toLowerCase() == "rock") : console.log("loss")
+        console.log(gameChoice + " beats " + playerChoice);
+        break;
+
+        case (playerChoice.toLowerCase() == "paper" && gameChoice.toLowerCase() == "scissors"): console.log("loss");
+        console.log(gameChoice + " beats " + playerChoice);
+        break;
+
+        case (playerChoice.toLowerCase() == "rock" && gameChoice.toLowerCase() == "paper"): console.log("loss");
+        console.log(gameChoice + " beats " + playerChoice);
+        break;
+    }
 }
+
+let i = 0;
+while (i < 5) {
+    gameMaster();
+    i++;
+}
+i = 0;
