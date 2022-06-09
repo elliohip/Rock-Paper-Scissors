715281eb7a8ada2ec4b24360b09142ec6fd691a7
Elliot Hipski <elhip03@gmail.com>
2022-06-09 15:12:15 -0400

    Add RPS.js

--- RPS.js
+++ RPS.js
@@ -1,7 +1,24 @@
+const startBtn = document.getElementById('btn');
+const rock = document.getElementById('rock');
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+
+
+
+const PICTURES = document.querySelectorAll(".images");
+const CHOICES = document.querySelectorAll(".choices");
+let CLOAK = document.getElementById("cloak");
+
+
+let gameChoice = computerPlay();
+let playerScore = 0;
+let computerScore = 0;
+let playerChoice;
+
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max)
-}
+};
 
 function computerPlay() {
     let choice = getRandomInt(3);
@@ -16,16 +33,50 @@ function computerPlay() {
         return "scissors";
     }
 
+};
+
+
+startBtn.addEventListener("click", changeOpac);
+
+function changeOpac() {
+    CLOAK.style.opacity = "1";
+
 }
 
-function gameMaster() {
 
-    let playerChoice = prompt("Rock, Paper, Scissors !!!");
+rock.addEventListener('click', getRock );
 
-    let gameChoice = computerPlay();
-    console.log("choose rock paper or scissors!");
+function getRock() {
+    playerChoice = "rock";
+    // set HTML of textbox
+}
 
-    let tie = (playerChoice.toLowerCase() == gameChoice.toLowerCase());
+paper.addEventListener("click", getPaper);
+
+function getPaper() {
+    playerChoice = "paper";
+    // set HTML of textbox
+}
+
+scissors.addEventListener("click", getScissors);
+
+function getScissors() {
+    playerChoice = "scissors";
+    // set HTML of textbox
+}
+
+// */
+
+
+
+function playGame() {
+
+    
+
+    let pScore;
+    let cScore;
+
+    let tie = (pChoice.toLowerCase() == gameChoice.toLowerCase());
 
     switch(true) {
 
@@ -34,33 +85,43 @@ function gameMaster() {
 
         case (playerChoice.toLowerCase() == "scissors" && gameChoice.toLowerCase() == 'paper'): console.log('win');
         console.log(playerChoice + " beats " + gameChoice);
+        pScore = true;
+        playerScore++;
         break;
 
         case (playerChoice.toLowerCase() == "paper" && gameChoice.toLowerCase() == "rock"): console.log("win");
         console.log(playerChoice + " beats " + gameChoice);
+        pScore = true;
+        playerScore++;
         break;
 
         case (playerChoice.toLowerCase() == "rock" && gameChoice.toLowerCase() == "scissors"): console.log("win");
         console.log(playerChoice + " beats " + gameChoice);
+        playerScore++;
+        pScore = true;
         break;
 
         case (playerChoice.toLowerCase() == "scissors" && gameChoice.toLowerCase() == "rock") : console.log("loss")
         console.log(gameChoice + " beats " + playerChoice);
+        gameScore++;
+        pScore = false;
         break;
 
         case (playerChoice.toLowerCase() == "paper" && gameChoice.toLowerCase() == "scissors"): console.log("loss");
         console.log(gameChoice + " beats " + playerChoice);
+        gameScore++;
+        pScore = false;
         break;
 
         case (playerChoice.toLowerCase() == "rock" && gameChoice.toLowerCase() == "paper"): console.log("loss");
         console.log(gameChoice + " beats " + playerChoice);
+        gameScore++;
+        pScore = false;
         break;
     }
+    return pScore;
 }
 
-let i = 0;
-while (i < 5) {
-    gameMaster();
-    i++;
+function gameMaster() {
+
 }
-i = 0;
